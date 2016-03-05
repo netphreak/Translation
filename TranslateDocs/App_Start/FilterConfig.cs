46f74482887001ec0196154151ad6d1a79a09296
JonTablet <anick.seve@gmail.com>
2016-03-05 03:29:06 -0700

    Added Authentication Module

---
+++ TranslateDocs/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TranslateDocs
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
