b15efa2e556c7cda1a3fe982748359c1ce82689a
netphreak <netphreak221@yahoo.com>
2016-03-05 02:00:26 -0700

    Project start

---
+++ TranslateDocs/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TranslateDocs.App_Start
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			//
+			// Scripts
+			//
+
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+				"~/Scripts/jquery-{version}.js"
+			));
+
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+				"~/Scripts/modernizr-*"
+			));
+
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+				"~/Scripts/bootstrap.js"
+			));
+
+			bundles.Add(new StyleBundle("~/Content/cssbundle").Include(
+				"~/Content/bootstrap.css"
+				, "~/Content/bootstrap-theme.css"
+				, "~/Content/Site.css"
+			));
+
+		}
+	}
+}
---
+++ TranslateDocs/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TranslateDocs.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+	    public ActionResult Import(HttpPostedFileBase importedFile)
+		{
+			string[] allowedTypes = {"text/plain", "application/rtf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" };
+			string[] allowedExtensions = {".txt", "doc", "docx", "rtf"};
+			
+			if (importedFile == null || importedFile.FileName == null) ModelState.AddModelError("Import", "No File");
+			else if (!allowedTypes.Contains(importedFile.ContentType)) ModelState.AddModelError("Import", "Invalid File Content");
+			else if (!allowedExtensions.Contains(Path.GetExtension(Path.GetFileName(importedFile.FileName).ToLower()))) ModelState.AddModelError("Import", "Invalid File Extension");
+			else if (importedFile.ContentLength == 0) ModelState.AddModelError("Import", "Empty File");
+			else
+			{
+				
+			}
+			return View("Index");
+		}
+    }
+}
